6d50bf164faba540defc25200edc8ba517acd894
Astrikkk <101702563+Astrikkk@users.noreply.github.com>
2024-07-06 13:20:48 +0300

    nigga1

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/categories', [CategoryController::class, 'getList']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+Route::post('/categories/{id}', [CategoryController::class, 'update']);
+
+Route::get('/products', [ProductController::class, 'getAll']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
